6aaf0289268ef2c3861e03433a39e5c1bfe172e1
Eliakin Alves <eliakin.alves.dev@gmail.com>
2019-10-17 13:31:05 -0300

    templates

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include "header.php"; ?>
+    <h1>pagina inicial</h1>
+    <p><a href="produtos.php">Ir para produtos</a></p>
+<?php include "footer.php"; ?>
---
+++ produtos.php
@@ -0,0 +1,4 @@
+<?php include "header.php"; ?>
+    <h1>pagina de produtos</h1>
+    <p><a href="index.php">Voltar para pagina inicial</a></p>
+<?php include "footer.php"; ?>
